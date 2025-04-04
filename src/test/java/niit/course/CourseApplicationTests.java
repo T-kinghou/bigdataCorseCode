0e7e526e0b18b99be4c7c3c3487088fa4880ca8e
T-kinghou <121077472+T-kinghou@users.noreply.github.com>
2025-04-04 19:58:05 +0800

    第一次项目代码提交

---
+++ src/test/java/niit/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package niit.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
